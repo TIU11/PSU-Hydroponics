ee54c859a1decda0646f374b8532c759da48031a
cwatson <cwatson@tiu11.org>
2021-06-23 19:35:55 +0100

    added code

---
+++ sketch_jun22a.ino
@@ -0,0 +1,46 @@
+
+int pump = 3;
+int sol = 4;
+int water = 5;
+int nut = 6;
+void setup() {
+  pinMode(pump,OUTPUT);
+  pinMode(sol,OUTPUT);
+  pinMode(water,INPUT);
+  pinMode(nut,OUTPUT);
+  //digitalWrite(pump,0);
+  //digitalWrite(sol,0);
+  
+  digitalWrite(nut,0);
+  delay(2000);
+  digitalWrite(nut,1);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(sol,1);
+  digitalWrite(pump,1);
+  addWater();
+
+}
+
+void addWater(){
+  while(digitalRead(water) == 0){
+    digitalWrite(pump,0);
+      digitalWrite(sol,0);
+
+      delay(5000);
+      digitalWrite(sol,1);
+
+      delay(30000);
+      
+      digitalWrite(sol,0);
+      
+      delay(10000);
+  }
+}
+
+
+
